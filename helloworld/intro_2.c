39d9f8f8b665ed1a6596ae00bf458ae839fd5a4c
morgane-naibo <morgane.naibo@gmail.com>
2023-08-19 16:05:51 +0200

    V2

--- helloworld/intro_2.c
+++ helloworld/intro_2.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
+#include <conio.h>
 int main(int argc, char const *argv[])
 {   
-    int a=10;
-    int b=15;
-    printf("%d", (a>b)? a: b);
+    int c=10;
+    int d=15;
+    printf("%d", (c>d)? c: d);
+    
+    int a,b;
+    char ch;
+    printf("Add, Substract, Multiply or Divide \n");
+    
+    do { /* continue tant que t'as pas de conditions du while */
+        printf("Enter first letter:  ");
+        ch = getchar();
+        printf("\n");
+    }   while (ch!='A' && ch!='S' && ch!='M' && ch!='D');
+    printf("Enter first number: ");
+    scanf("%d", &a);
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    switch(ch){
+        case 'A' :  printf("%d", a+b);
+                    break;
+        case 'S' :  printf("%d", a-b);
+                    break;
+        case 'M' :  printf("%d", a*b);
+                    break;
+        case 'D' :  if (b!=0) printf("%d", a/b);
+                    break;
+                      }    
 }
